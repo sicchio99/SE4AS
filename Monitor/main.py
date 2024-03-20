fb5152dd985087c5ace1dbf88d75b703bcae5e72
sicchio99 <giovannisicchio@gmail.com>
2024-03-20 21:18:07 +0100

    Test broker mqtt

--- Monitor/main.py
+++ Monitor/main.py
@@ -1,35 +1,29 @@
 import paho.mqtt.client as mqtt
-import time
 
 
-def on_connect(self,client,userdata,flags,rc):
-    print("Connected with result code "+str(rc))
-    client.subscrive("se4as/test/test2")
-    return rc
+def on_connect(client, userdata, flags, reason_code, properties):
+    if reason_code.is_failure:
+        print(f"Failed to connect: {reason_code}. loop_forever() will retry connection")
+    else:
+        client.subscribe("se4as/test/#")
+
+
+def on_subscribe(client, userdata, mid, reason_code_list, properties):
+    if reason_code_list[0].is_failure:
+        print(f"Broker rejected you subscription: {reason_code_list[0]}")
+    else:
+        print(f"Broker granted the following QoS: {reason_code_list[0].value}")
 
 
 def on_message(client, userdata, message):
-    time.sleep(2)
+    client.publish("new", message.payload, qos=1)
     client.publish("se4as/test/test3", message.payload, qos=1)
 
 
 if __name__ == '__main__':
-    #mqtt_pub = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, reconnect_on_failure=True)
-    #mqtt_pub.connect("mosquitto", 1883)
-
     mqttc = mqtt.Client(mqtt.CallbackAPIVersion.VERSION2, reconnect_on_failure=True)
     mqttc.on_connect = on_connect
     mqttc.on_message = on_message
+    mqttc.on_subscribe = on_subscribe
     mqttc.connect("mosquitto", 1883)
-    #mqttc.loop_forever()
-    mqttc.loop_start()
-    while True:
-        a = 1
-        print(a)
-
-    # Ciclo while True per mantenere il client MQTT in esecuzione
-    #while True:
-        #if len(messages)>0:
-            #print("Actual: "+str(messages))
-            #mqttc.publish("se4as/test/test3", messages[-1], qos=1)
-            #time.sleep(2)
+    mqttc.loop_forever()
